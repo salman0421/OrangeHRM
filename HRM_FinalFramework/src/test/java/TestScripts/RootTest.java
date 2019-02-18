589b2b5686ea41e615ace8de916140938e63058d
QAP16 <QAP16@QAP16-PC>
2019-02-18 18:39:26 +0530

    sd

--- HRM_FinalFramework/src/test/java/TestScripts/RootTest.java
+++ HRM_FinalFramework/src/test/java/TestScripts/RootTest.java
@@ -10,6 +10,6 @@ public class RootTest
 	
 	static Common brow=new Common();
 	static Properties locators=new Properties();
-	static String locfile="C:\\Users\\QAP16\\Desktop\\Hari Selenium\\HRM_FinalFramework\\src\\main\\java\\ElementLocators\\Login.properties";
+	static String locfile="C:\\Users\\QAP16\\git\\OrangeHRM\\HRM_FinalFramework\\src\\main\\java\\ElementLocators\\Login.properties";
   
 }
